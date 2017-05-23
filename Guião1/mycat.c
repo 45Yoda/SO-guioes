0c2c4e53cc9ca9375ae962de1d1f8a3d7871ac8e
Yoda45 <ninjadinosaur.ilikecake@gmail.com>
2017-05-23 16:18:49 +0100

    cenas

--- Guião1/mycat.c
+++
@@ -1,9 +0,0 @@
-#include <unistd.h>
-#include <fcntl.h>
-
-int main(){
-    char c;
-    while (read(0,&c,1) >0){
-        write(1,&c,1);
-    }
-}
--- Guião2/ex1.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <unistd.h>
-
-int main(){
-    printf("Filho - %d\n",getpid());
-    printf("Pai   - %d\n",getppid());
-    return 0;
-}
